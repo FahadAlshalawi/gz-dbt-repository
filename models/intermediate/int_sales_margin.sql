1befdd58424b54df206f6c616e8544648e27be36
Fahad Alshalawi <fahd.alshal@gmail.com>
2024-03-06 09:01:48 +0000

    jfoiwper

--- models/intermediate/int_sales_margin.sql
+++ models/intermediate/int_sales_margin.sql
@@ -1,15 +1,12 @@
-    select *
-    from (
-        SELECT 
-    products_id, 
-    date_date, 
+SELECT
+    products_id,
+    date_date,
     orders_id,
-    revenue, 
-    quantity, 
-    CAST(purchase_price AS FLOAT64) AS purchase_price,  
+    revenue,
+    quantity,
+    CAST(purchase_price AS FLOAT64) AS purchase_price,
     ROUND(s.quantity*CAST(p.purchase_price AS FLOAT64),2) AS purchase_cost,
     s.revenue - ROUND(s.quantity*CAST(p.purchase_price AS FLOAT64),2) AS margin
-FROM `idyllic-anvil-414008`.`dbt_fahad`.`stg_raw__sales` s
-LEFT JOIN `idyllic-anvil-414008`.`dbt_fahad`.`stg_raw__product` p 
+FROM {{ref("stg_raw__sales")}} s
+LEFT JOIN {{ref("stg_raw__product")}} p
     USING (products_id)
-    ) as model_limit_subq
