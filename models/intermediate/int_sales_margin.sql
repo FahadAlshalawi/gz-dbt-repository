b9b214f7b499edf709ea76607524ce23d902d856
Fahad Alshalawi <fahd.alshal@gmail.com>
2024-03-06 08:56:36 +0000

    khiuh

--- models/intermediate/int_sales_margin.sql
+++ models/intermediate/int_sales_margin.sql
@@ -6,7 +6,7 @@
     orders_id,
     revenue, 
     quantity, 
-    CAST(purchase_price AS FLOAT64) AS purchase_price, 
+    CAST(purchase_price AS FLOAT64) AS purchase_price,  
     ROUND(s.quantity*CAST(p.purchase_price AS FLOAT64),2) AS purchase_cost,
     s.revenue - ROUND(s.quantity*CAST(p.purchase_price AS FLOAT64),2) AS margin
 FROM `idyllic-anvil-414008`.`dbt_fahad`.`stg_raw__sales` s
